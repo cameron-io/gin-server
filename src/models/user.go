443d768136a5b4d8ff01aa2c57094a9093479ed6
Cameron Gallichan <camerongallichan@gmail.com>
2024-08-12 14:44:39 +0100

    refactoring: adjust indents

--- src/models/user.go
+++ src/models/user.go
@@ -1,8 +1,8 @@
 package models
 
 type User struct {
-	Name      string `json:"name" validate:"min=3,max=50"`
-	Email     string `json:"email" validate:"required,email"`
+	Name      string `json:"name"     validate:"min=3,max=50"`
+	Email     string `json:"email"    validate:"required,email"`
 	Password  string `json:"password" validate:"required,min=10,max=50"`
 	Avatar    string `json:"avatar"`
 	CreatedAt int64  `json:"created_at"`
