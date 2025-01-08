5572e8621a026945d84fb742d9dfcbd078200dea
Cameron Gallichan <camerongallichan@gmail.com>
2025-01-08 22:06:29 +0000

    add cors config

--- main.go
+++ main.go
@@ -3,16 +3,27 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"cameron.io/gin-server/api/controllers"
 	"cameron.io/gin-server/api/middleware"
 	"cameron.io/gin-server/application/services"
 	jwt "github.com/appleboy/gin-jwt/v2"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://github.com"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	rGroupApi := r.Group("/api")
 
